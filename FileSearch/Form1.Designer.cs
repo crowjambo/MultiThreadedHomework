0640680ee18e780728ddedb2600ceff153de6bbe
crowjambo <evaldascrow@gmail.com>
2020-02-21 07:23:52 +0200

    task done

--- FileSearch/Form1.Designer.cs
+++ FileSearch/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(12, 38);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(473, 20);
+            this.directoryTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(133, 136);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(227, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 92);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(473, 20);
+            this.fileNameTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory: (C:\\ will crash due to UNAUTHORIZEDEXCEPTION, so use something else)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Filename: (file endings matters, for example: someFile.txt instead of someFile)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(502, 228);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.directoryTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Task1 - File/directory lister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox directoryTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FileSearch/Form1.cs
+++ FileSearch/Form1.cs
@@ -7,14 +7,153 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
+/*
+ * input
+ * The name of a searched item;
+   Search initial folder.
+ * 
+ * output
+ * list of folders there the searched item is found.
+   search duration.
+ */
 
 namespace FileSearch
 {
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Sets the progress bar value, without using Windows Aero animation
+        /// </summary>
+        public static void SetProgressNoAnimation(this ProgressBar pb, int value)
+        {
+            // Don't redraw if nothing is changing.
+            if (value == pb.Value)
+                return;
+
+            // To get around this animation, we need to move the progress bar backwards.
+            if (value == pb.Maximum)
+            {
+                // Special case (can't set value > Maximum).
+                pb.Value = value;           // Set the value
+                pb.Value = value - 1;       // Move it backwards
+            }
+            else
+            {
+                pb.Value = value + 1;       // Move past
+            }
+            pb.Value = value;               // Move to correct value
+        }
+    }
+
+    public struct WorkerData
+    {
+        public string directories;
+        public double time;
+    }
+
     public partial class Form1 : Form
     {
+
+        BackgroundWorker worker = new BackgroundWorker();
+
         public Form1()
         {
             InitializeComponent();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_Completed;
         }
+
+        private void Worker_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var result = (WorkerData)e.Result;
+            MessageBox.Show($"{result.directories} - Time: {result.time} ms");
+            button1.Enabled = true;
+            progressBar1.Value = 0;
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var results = SearchFile(directoryTextBox.Text, fileNameTextBox.Text);
+            e.Result = results;
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ExtensionMethods.SetProgressNoAnimation(progressBar1, e.ProgressPercentage);
+        }
+
+        private WorkerData SearchFile(string directory, string fileName)
+        {
+            WorkerData outputData = new WorkerData();
+  
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            
+            string rootPath = @directory;
+            string[] files = Directory.GetFiles(rootPath, fileName, SearchOption.AllDirectories);
+           
+            if(files.Length !=0)
+            {
+                string file = files[0];
+                rootPath = Path.GetDirectoryName(file);
+            }
+            else
+            {
+                return new WorkerData();
+            }
+            
+            string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.TopDirectoryOnly);
+            string outputDirectories = String.Empty;
+
+            float totalDirs = dirs.Length;
+            float counter = 1f;
+
+            foreach (string dir in dirs)
+            {
+                int progress = CheckProgressBarInput(((counter / totalDirs) * 100));
+                outputDirectories += $"count: {counter} - name: {dir} \n";
+                counter++;
+                worker.ReportProgress(CheckProgressBarInput(progress));
+            }
+
+            watch.Stop();
+
+            outputData.directories = outputDirectories;
+            outputData.time = watch.Elapsed.TotalMilliseconds;
+            return outputData;
+        }
+
+        private int CheckProgressBarInput(float value)
+        {
+            if (value > 100) return 100;
+            else return (int)value;
+        }
+
+        private bool CheckTextBoxInput()
+        {
+            if (directoryTextBox.Text.Length == 0 || fileNameTextBox.Text.Length == 0) return false;
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CheckTextBoxInput())
+            {
+                worker.RunWorkerAsync();
+                button1.Enabled = false;
+            } else
+            {
+                MessageBox.Show("Bad inputs");
+            }
+
+        }
+
     }
 }
+
+
